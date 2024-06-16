a542d12163dfd6e7e6f14400f5eb6ed94a6969fa
stephane-klein <contact@stephane-klein.info>
2024-06-16 17:06:07 +0200

    Revert previous commit, Age function are accessible by ag_catalog schema and not graph schema

--- docker-entrypoint-initdb.d/00-create-extension-age.sql
+++ docker-entrypoint-initdb.d/00-create-extension-age.sql
@@ -1,4 +1,4 @@
 CREATE EXTENSION age;
 LOAD 'age';
-SET search_path = ag_catalog, "$user", public, graph;
-ALTER DATABASE postgres SET search_path TO ag_catalog, "$user", public, graph;
+SET search_path = ag_catalog, "$user", public;
+ALTER DATABASE postgres SET search_path TO ag_catalog, "$user", public;
