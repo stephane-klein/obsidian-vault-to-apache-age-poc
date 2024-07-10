c9751959a3f92c875dd6a12c54bde8d671aa74f9
stephane-klein <contact@stephane-klein.info>
2024-07-10 22:25:49 +0200

    WIP

---
+++ typesense-search.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+import Typesense from "typesense";
+
+let client = new Typesense.Client({
+    'nodes': [{
+        'host': '127.0.0.1',
+        'port': 8108,
+        'protocol': 'http'
+    }],
+    'apiKey': 'xyz',
+    'connectionTimeoutSeconds': 2
+})
+
+let result = await client.collections("notes")
+    .documents()
+    .search({
+        "q": "Codemirror",
+        "query_by": "content"
+    });
+
+console.dir(result, { depth: null });
